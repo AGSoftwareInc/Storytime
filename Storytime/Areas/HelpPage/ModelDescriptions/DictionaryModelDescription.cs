890bb8190cb05ad8d24d5d29ed50b73801652074
gpartin <gpartin@gmail.com>
2016-02-10 18:21:59 -0500

    Initial Commit

---
+++ Storytime/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Storytime.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Storytime/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Storytime.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IHttpActionResult GetUser(string UserId)
+        {
+            var db = new PetaPoco.Database("AGSoftware");
+
+            var a = db.SingleOrDefault<Entities.User>("Select * from [User] Where UserId = @0", UserId);
+            return Ok(a);
+
+        }
+    }
+}

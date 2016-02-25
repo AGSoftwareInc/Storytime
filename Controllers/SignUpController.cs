08e5561752b821c44291e8e7cfc91508dde793aa
gpartin <gpartin@gmail.com>
2016-02-25 10:47:55 -0500

    Add storytime controller

---
+++ Controllers/SignUpController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Storytime.Controllers
+{
+    public class SignUpController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post([FromUri] string id, [FromBody]Entities.User user)
+        {
+            user.DateCreated = System.DateTime.Now;
+            var db = new PetaPoco.Database("AGSoftware");
+
+            db.Insert(user);
+
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/StoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Storytime.Controllers
+{
+    public class StoryController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post([FromUri] string id, [FromBody]Entities.StorytimePost storytimepost)
+        {
+            storytimepost.DateCreated = System.DateTime.Now;
+            var db = new PetaPoco.Database("AGSoftware");
+
+            db.Insert(storytimepost);
+
+            return Ok();
+        }
+
+    [HttpGet]
+        public IHttpActionResult Get(string id)
+        {
+            var db = new PetaPoco.Database("AGSoftware");
+
+            var a = db.SingleOrDefault<Entities.StorytimePost>("Select * from StoryTimePost Where StorytimePostId = @0", id);
+            return Ok(a);
+        }
+    }
+}

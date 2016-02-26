8532464bc5c4e2400c2796694715894968636f0a
gpartin <gpartin@gmail.com>
2016-02-26 13:36:11 -0500

    Added Image controller

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Storytime.Controllers
+{
+    public class ImageController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post([FromUri] string id, [FromBody]HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(System.Web.HttpContext.Current.Server.MapPath("~/images/profile"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+
+                // save the image path path to the database or you can send image 
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}

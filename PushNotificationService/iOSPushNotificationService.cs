c5c9a8d56cc62db6fdcf6356ff15fc490306d5a2
gpartin <gpartin@gmail.com>
2016-03-28 16:15:38 -0400

    Added service for connection to iOS push notifcation

---
+++ PushNotificationService/iOSPushNotificationService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using MoonAPNS;
+
+namespace PushNotificationService
+{
+    public partial class iOSPushNotificationService : ServiceBase
+    {
+        public iOSPushNotificationService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var payload1 = new NotificationPayload("a961e67e650a5eb5610027e346d78c4df263ef8f", "Hello !", 1, "default");
+            //payload1.AddCustom("CustomKey", "CustomValue");
+            var notificationList = new List<NotificationPayload> { payload1 };
+            var push = new PushNotification(false, "C:\\OpenSSL\\bin\\aps_development.cer", "test");
+            var rejected = push.SendToApple(notificationList);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}

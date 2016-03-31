84782f9786514deb5d654a6a0ea148fe4479a5d3
gpartin <gpartin@gmail.com>
2016-03-31 18:36:39 -0400

    Added LogError controller

---
+++ Entities/Logs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Logs
+    {
+        public int LogsId { get; set; }
+        public DateTime EventDateTime { get; set; }
+        public string EventLevel { get; set; }
+        public string UserName { get; set; }
+        public string MachineName { get; set; }
+        public string EventMessage { get; set; }
+        public string ErrorSource { get; set; }
+        public string ErrorClass { get; set; }
+        public string ErrorMethod { get; set; }
+        public string ErrorMessage { get; set; }
+        public string InnerErrorMessage { get; set; }
+    }
+}
---
+++ Web/Controllers/LogErrorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Storytime.Controllers
+{
+    [Authorize]
+    public class LogErrorController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post([FromUri] string id, [FromBody]Entities.Logs logs)
+        {
+            var db = new PetaPoco.Database("AGSoftware");
+
+            logs.EventDateTime = System.DateTime.Now;
+            logs.UserName = this.User.Identity.Name;
+
+
+            db.Insert(logs);
+
+            return Ok();
+        }
+    }
+}

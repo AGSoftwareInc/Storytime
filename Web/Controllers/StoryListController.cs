6b96e8484a00508aec01b35b783e0d53bf97128b
gpartin <gpartin@gmail.com>
2016-04-05 09:05:22 -0400

    Added storytimepostlist controller and updated storylist controller

--- Web/Controllers/StoryListController.cs
+++ Web/Controllers/StoryListController.cs
@@ -16,24 +16,21 @@ namespace Storytime.Controllers
         {
             var db = new PetaPoco.Database("AGSoftware");
 
-            var b = db.SingleOrDefault<Entities.Storytime>("Select * from Storytime Where StorytimeId = @0", id);
+            System.Collections.Generic.List<Entities.Storytime> storytimelist = new List<Entities.Storytime>();
 
-            if (b != null)
+            foreach (var c in db.Query<Entities.Storytime>("Select * from StorytimePost Where UserId = @0", Storytime.Providers.UserHelper.GetUserId(this.User.Identity.Name)))
             {
-                System.Collections.Generic.List<Entities.StorytimePost> storytimepostlist = new List<Entities.StorytimePost>();
+                storytimelist.Add(c);
+            }
 
-
-                foreach (var c in db.Query<Entities.StorytimePost>("Select * from StorytimePost Where StorytimeId = @0", id))
-                {
-                    c.ImagePath = c.ImagePath.Replace("C:\\Storytime\\Web\\", "http://" + System.Configuration.ConfigurationManager.AppSettings["Server"] + @"\");
-                    c.ImagePath = c.ImagePath.Replace(@"\", @"/");
-                    storytimepostlist.Add(c);
-                }
-                return Ok(storytimepostlist);
+            if (storytimelist.Count > 0)
+            {
+                return Ok(storytimelist);
             }
             else
+            {
                 return NotFound();
-
+            }
         }
     }
 }
---
+++ Web/Controllers/StorytimePostListController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Storytime.Controllers
+{
+    [Authorize]
+    public class StorytimePostListController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get(string id)
+        {
+            var db = new PetaPoco.Database("AGSoftware");
+
+            System.Collections.Generic.List<Entities.StorytimePost> storytimepostlist = new List<Entities.StorytimePost>();
+
+            foreach (Entities.StorytimePost c in db.Query<Entities.StorytimePost>("Select * From StorytimePost Where StorytimeId = @0", id))
+            {
+                c.ImagePath = c.ImagePath.Replace("C:\\Storytime\\Web\\", "http://" + System.Configuration.ConfigurationManager.AppSettings["Server"] + @"\");
+                c.ImagePath = c.ImagePath.Replace(@"\", @"/");
+
+                storytimepostlist.Add(c);
+            }
+
+            if (storytimepostlist.Count > 0)
+            {
+                return Ok(storytimepostlist);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}

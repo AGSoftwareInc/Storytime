e69da85ed984a37e2b092baa6cb341d45f5932f0
gpartin <gpartin@gmail.com>
2016-04-06 09:45:22 -0400

    Updated storylist controller to pull back all stories a user has created, is in a group for a story another user created or is in the user list of a story another user created.

--- Entities/Storytime.cs
+++ Entities/Storytime.cs
@@ -11,11 +11,16 @@ namespace Entities
         public int StorytimeId { get; set; }
         public string StorytimeTitle { get; set; }
         public DateTime DateCreated { get; set; }
-        private int storytimetypeid; 
+        private int storytimetypeid;
+        private StorytimeType storytimetype;
         public int StorytimeTypeId
         {
             get { return (int)this.StorytimeType; }
-            set { this.storytimetypeid = value; }
+            set
+            { 
+                this.storytimetypeid = value;
+                this.storytimetype = (Entities.StorytimeType)value;
+            }
         }
         public string UserId { get; set; }
 
@@ -29,7 +34,11 @@ namespace Entities
         public int StorytimeGroupId { get; set; }
 
         [PetaPoco.Ignore]
-        public StorytimeType StorytimeType { get; set; }
+        public StorytimeType StorytimeType 
+        {
+            get { return this.storytimetype; }
+            set { this.storytimetype = value; }
+        }
 
     }
 }
